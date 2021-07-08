253537481b4d86645ad2a2d137a3d56eda576f31
JoshuaWengraf <64872799+JoshuaWengraf@users.noreply.github.com>
2021-07-08 17:59:34 +0100

    Ball now accellerates towards mouse

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 const blue = "#0000ff"
-
-document.addEventListener('mousemove', (event) => {
-    x = event.clientX;
-    y = event.clientY;
-
-    console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`);
-});	
-
-var canvas = document.getElementById("canvas");
-  
-function draw(){
-
-
-    context = canvas.getContext('2d');
-	context.clearRect(0,0,300,300);
-
-    context.beginPath();
+let drawCircle = function (x, y, radius) {
+	context.beginPath();
 	context.fillStyle = blue;
-	context.arc(x, y, 20, 0, Math.PI * 2, false);
+	context.arc(x, y, radius, 0, Math.PI * 2, false);
 	context.closePath();
-	context.fill();
+}
+
+let drawWindow = function() {
+	context.clearRect(0,0,300,300);
+}
+
+let canvas = document.getElementById("canvas");
+context = canvas.getContext('2d');
+
+x = 0;
+y = 0;
+
+function draw(){
+	document.addEventListener('mousemove', (event) => {
+		xMouse = event.clientX;
+		yMouse = event.clientY;
+	});	
+
+	deltaX = xMouse - x; //calculate vector pointing from ball to mouse
+	deltaY = yMouse - y; 
+
+	x = x + 0.01 * deltaX;
+	y = y + 0.01 * deltaY;
+
+	drawWindow();
+    drawCircle(x,y,5);
 };
 
 setInterval(draw,10); 
